550db6e0f8169dd80b78876c5d458ae230456c4d
István Pató <istvan.pato@gmail.com>
2022-12-23 15:00:38 +0100

    chore: init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('tw-zen')],
+  safelist: ['zen--suspend', 'zen--animate', 'zen--reduced']
+}
