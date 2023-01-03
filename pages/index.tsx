4ce6fd7709db3e190301dc9db54e60eeb27e490c
István Pató <istvan.pato@gmail.com>
2023-01-03 12:41:52 +0100

    fix: add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>TW Zen example with Next.js</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
